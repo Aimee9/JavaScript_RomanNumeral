7a6c0bf09457400015d5522ba32a6475b73090e2
Aimee Reiss & Ashley Sullins <student@epicodus.com>
2015-09-16 16:11:25 -0700

    Add tests for numbers 4 and 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,62 @@
 var getNumerals = function(numbers) {
-  //  var biggestLittle; //holds the largest qualifying Roman numeral which is <= (numbers)
   var romanNumeral = [];
   var numerals;
-  while (numbers > 0) {
 
-    if(numbers >= 1000){
+
+    while(numbers >= 1000){
       romanNumeral.push("M");
       numbers -= 1000;
     }
 
-    if(numbers >= 500){
+    while(numbers >= 500){
       romanNumeral.push("D");
       numbers -= 500;
     }
 
-    if(numbers >= 100){
+    while(numbers >= 100){
       romanNumeral.push("C");
       numbers -= 100;
     }
 
-    if(numbers >= 50){
+    while(numbers >= 50){
       romanNumeral.push("L");
       numbers -= 50;
     }
 
-    if(numbers >= 10){
+    while(numbers === 40){
+      romanNumeral.unshift("XL");
+      numbers -= 40;
+    }
+
+    while(numbers >= 10){
       romanNumeral.push("X");
       numbers -= 10;
     }
 
-    if(numbers >= 5){
+    while(numbers === 9){
+      romanNumeral.unshift("I");
+      numbers -= 9;
+    }
+
+    while(numbers >= 5){
       romanNumeral.push("V");
       numbers -= 5;
     }
-    if(numbers >= 1){
+
+    while(numbers === 4){
+      romanNumeral.push("IV");
+      numbers -= 4;
+    }
+
+    while(numbers >= 1){
       romanNumeral.push("I");
       numbers -= 1;
     }
-  }
    numerals = romanNumeral.join().replace(/[,]/g,"");
 
   return numerals;
 };
 
-// //returns largest roman numeral <= numbers
-//  var getBiggestLittle = function(numbers) {
-//    var character;
-//
-//   if(numbers >= 1000){
-//     numbers = numbers - 1000;
-//      character = "M";
-//     }
-//
-//    else if(numbers >= 500){
-//      numbers = numbers - 500;
-//         character = "D";
-//     }
-//
-//    else if(numbers >= 100){
-//      numbers = numbers - 100;
-//          character = "C";
-//     }
-//
-//    else if(numbers >= 50){
-//      numbers = numbers - 50;
-//           character = "L";
-//     }
-//
-//    else if(numbers >= 10){
-//      numbers = numbers - 10;
-//            character = "X";
-//     }
-//
-//    else if(numbers >= 5){
-//      numbers = numbers - 5;
-//             character = "V";
-//     }
-//
-//    else if(numbers >= 1){
-//      numbers = numbers - 1;
-//              character = "I";
-//     }
-//
-//
-//   return character;
-//  };
 
 $(document).ready(function() {
   $("form#RomanNumerals").submit(function(event) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,51 +1,23 @@
-// describe('getBiggestLittle', function() {
-//   it("is true for a 1000 that is a roman numeral", function() {
-//     expect(getBiggestLittle(1000)).to.equal("M");
-//   });
-//
-//     it("is true for a 500 that is a roman numeral", function() {
-//       expect(getBiggestLittle(500)).to.equal("D");
-//     });
-//
-//     it("is true for a 100 that is a roman numeral", function() {
-//       expect(getBiggestLittle(100)).to.equal("C");
-//     });
-//
-//     it("is true for a 50 that is a roman numeral", function() {
-//       expect(getBiggestLittle(50)).to.equal("L");
-//     });
-//
-//     it("is true for a 10 that is a roman numeral", function() {
-//       expect(getBiggestLittle(10)).to.equal("X");
-//     });
-//
-//     it("is true for a 5 that is a roman numeral", function() {
-//       expect(getBiggestLittle(5)).to.equal("V");
-//     });
-//
-//     it("is true for a 1 that is a roman numeral", function() {
-//       expect(getBiggestLittle(1)).to.equal("I");
-//     });
-//
-//     it("is true for a 15 that is a roman numeral", function() {
-//       expect(getBiggestLittle(15)).to.equal("X");
-//     });
-
-
-  // it("is true for 2 is II", function() {
-  //   expect(getNumerals(2)).to.equal("II");
-  //
-  // });
-  //
-  // it("is true for 3 is III", function() {
-  //   expect(getNumerals(3)).to.equal("III");
-  //
-  // });
-// });
 
 describe('getNumerals', function() {
   it("is true for a 15 that is a roman numeral", function() {
     expect(getNumerals(15)).to.equal("XV");
+  });
+
+  it("will know how to correctly make a roman numeral with a 9", function() {
+    expect(getNumerals(29)).to.equal("IXX");
+  });
+
+  it("will know how to correctly make a roman numeral with a 4", function() {
+    expect(getNumerals(34)).to.equal("XXXIV");
+  });
+
+  it("will know how to correctly make a roman numeral with the number 40", function() {
+    expect(getNumerals(40)).to.equal("XL");
+  });
+
+  it("will know how to correctly make a roman numeral with the number 80", function() {
+    expect(getNumerals(80)).to.equal("LXXX");
   });
 
 });
