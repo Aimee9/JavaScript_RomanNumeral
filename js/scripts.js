aa7f43a7fbd3cd8093222f28df20cc87e6e58d81
colleen minor & teresa fletcher <student@epicodus.com>
2015-09-16 11:29:33 -0700

    New route of passage to numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 var getNumerals = function(numbers) {
 
-  var romanNumeral;
-  romanNumeral = "I";
-  return romanNumeral;
+  var romanNumeral = new Array();
+
+    for(var i = 0; i < numbers; i++) {
+       if (number <= 3999;)
+       ;
+    }
 
 
-};
+
+//     var numerals = romanNumeral.join();
+//    numerals = numerals.replace(/[,]/g,"");
+//
+//   return numerals;
+
+ };
 
 
 $(document).ready(function() {
   $("form#RomanNumerals").submit(function(event) {
     var numbers = parseInt($("input#numbers").val());
-  //  numbers = getnumerals(numbers);
-
-    $(".numbers").text(numbers);
+    //numbers = getnumerals(numbers);
+    $(".numeral").text(numerals);
 
 
      $("#result").show();
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,15 @@
 describe('getNumerals', function() {
-  it("is true for a single digit that is a roman numeral", function() {
-    expect(getNumerals(1)).to.equal("I");
+  it("is true for a 1000 that is a roman numeral", function() {
+    expect(getNumerals(1000)).to.equal("M");
   });
 
-  it("is true for 2 is II", function() {
-    expect(getNumerals(2)).to.equal("II");
-  });
+  // it("is true for 2 is II", function() {
+  //   expect(getNumerals(2)).to.equal("II");
+  //
+  // });
+  //
+  // it("is true for 3 is III", function() {
+  //   expect(getNumerals(3)).to.equal("III");
+  //
+  // });
 });
