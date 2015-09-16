1c4d1ef243251aa321adb64e924dfa106bed0bf5
Aimee Reiss & Ashley Sullins <student@epicodus.com>
2015-09-16 15:18:19 -0700

    Convert numbers to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,90 @@
 var getNumerals = function(numbers) {
-  var biggestLittle; //holds the largest qualifying Roman numeral which is <= (numbers)
-  var romanNumeral = new Array(); //will hold the roman numeral
+  //  var biggestLittle; //holds the largest qualifying Roman numeral which is <= (numbers)
+  var romanNumeral = [];
+  var numerals;
+  while (numbers > 0) {
 
-   while (numbers > 0){
-     biggestLittle = getBiggestLittle(numbers);
-     romanNumeral.push(biggestLittle);
-     if(biggestLittle === "M"){
-       numbers = numbers - 1000;
-       }
-       else if(biggestLittle === "D"){
-          numbers = numbers - 500;
-          }
-        else if(biggestLittle === "C"){
-           numbers = numbers - 100;
-         }
-         else if(biggestLittle === "L"){
-            numbers = numbers - 50;
-          }
-         else if(biggestLittle === "X"){
-            numbers = numbers - 10;
-          }
-         else if(biggestLittle === "V"){
-             numbers = numbers - 5;
-           }
-         else if(biggestLittle === "I"){
-            numbers = numbers - 1;
-            }
-
-          }
-          var numbers = romanNumeral.join();
-             numbers = numbers.replace(/[,]/g,"");
-
-          return numbers;
-
-
-
- };
-
-//returns largest roman numeral <= numbers
- var getBiggestLittle = function(numbers) {
-   var character;
-
-  if(numbers >= 1000){
-     character = "M";
+    if(numbers >= 1000){
+      romanNumeral.push("M");
+      numbers -= 1000;
     }
 
-   else if(numbers >= 500){
-        character = "D";
+    if(numbers >= 500){
+      romanNumeral.push("D");
+      numbers -= 500;
     }
 
-   else if(numbers >= 100){
-         character = "C";
+    if(numbers >= 100){
+      romanNumeral.push("C");
+      numbers -= 100;
     }
 
-   else if(numbers >= 50){
-          character = "L";
+    if(numbers >= 50){
+      romanNumeral.push("L");
+      numbers -= 50;
     }
 
-   else if(numbers >= 10){
-           character = "X";
+    if(numbers >= 10){
+      romanNumeral.push("X");
+      numbers -= 10;
     }
 
-   else if(numbers >= 5){
-            character = "V";
+    if(numbers >= 5){
+      romanNumeral.push("V");
+      numbers -= 5;
     }
-
-   else if(numbers >= 1){
-             character = "I";
+    if(numbers >= 1){
+      romanNumeral.push("I");
+      numbers -= 1;
     }
+  }
+   numerals = romanNumeral.join().replace(/[,]/g,"");
 
+  return numerals;
+};
 
-  return character;
- };
+// //returns largest roman numeral <= numbers
+//  var getBiggestLittle = function(numbers) {
+//    var character;
+//
+//   if(numbers >= 1000){
+//     numbers = numbers - 1000;
+//      character = "M";
+//     }
+//
+//    else if(numbers >= 500){
+//      numbers = numbers - 500;
+//         character = "D";
+//     }
+//
+//    else if(numbers >= 100){
+//      numbers = numbers - 100;
+//          character = "C";
+//     }
+//
+//    else if(numbers >= 50){
+//      numbers = numbers - 50;
+//           character = "L";
+//     }
+//
+//    else if(numbers >= 10){
+//      numbers = numbers - 10;
+//            character = "X";
+//     }
+//
+//    else if(numbers >= 5){
+//      numbers = numbers - 5;
+//             character = "V";
+//     }
+//
+//    else if(numbers >= 1){
+//      numbers = numbers - 1;
+//              character = "I";
+//     }
+//
+//
+//   return character;
+//  };
 
 $(document).ready(function() {
   $("form#RomanNumerals").submit(function(event) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,35 +1,35 @@
-describe('getBiggestLittle', function() {
-  it("is true for a 1000 that is a roman numeral", function() {
-    expect(getBiggestLittle(1000)).to.equal("M");
-  });
-
-    it("is true for a 500 that is a roman numeral", function() {
-      expect(getBiggestLittle(500)).to.equal("D");
-    });
-
-    it("is true for a 100 that is a roman numeral", function() {
-      expect(getBiggestLittle(100)).to.equal("C");
-    });
-
-    it("is true for a 50 that is a roman numeral", function() {
-      expect(getBiggestLittle(50)).to.equal("L");
-    });
-
-    it("is true for a 10 that is a roman numeral", function() {
-      expect(getBiggestLittle(10)).to.equal("X");
-    });
-
-    it("is true for a 5 that is a roman numeral", function() {
-      expect(getBiggestLittle(5)).to.equal("V");
-    });
-
-    it("is true for a 1 that is a roman numeral", function() {
-      expect(getBiggestLittle(1)).to.equal("I");
-    });
-
-    it("is true for a 15 that is a roman numeral", function() {
-      expect(getBiggestLittle(15)).to.equal("X");
-    });
+// describe('getBiggestLittle', function() {
+//   it("is true for a 1000 that is a roman numeral", function() {
+//     expect(getBiggestLittle(1000)).to.equal("M");
+//   });
+//
+//     it("is true for a 500 that is a roman numeral", function() {
+//       expect(getBiggestLittle(500)).to.equal("D");
+//     });
+//
+//     it("is true for a 100 that is a roman numeral", function() {
+//       expect(getBiggestLittle(100)).to.equal("C");
+//     });
+//
+//     it("is true for a 50 that is a roman numeral", function() {
+//       expect(getBiggestLittle(50)).to.equal("L");
+//     });
+//
+//     it("is true for a 10 that is a roman numeral", function() {
+//       expect(getBiggestLittle(10)).to.equal("X");
+//     });
+//
+//     it("is true for a 5 that is a roman numeral", function() {
+//       expect(getBiggestLittle(5)).to.equal("V");
+//     });
+//
+//     it("is true for a 1 that is a roman numeral", function() {
+//       expect(getBiggestLittle(1)).to.equal("I");
+//     });
+//
+//     it("is true for a 15 that is a roman numeral", function() {
+//       expect(getBiggestLittle(15)).to.equal("X");
+//     });
 
 
   // it("is true for 2 is II", function() {
@@ -41,7 +41,7 @@ describe('getBiggestLittle', function() {
   //   expect(getNumerals(3)).to.equal("III");
   //
   // });
-});
+// });
 
 describe('getNumerals', function() {
   it("is true for a 15 that is a roman numeral", function() {
